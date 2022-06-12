74e06cc903e0c60992cfc5a24bddf7c8630d48f3
MatheusTrinca <matheusfelipetrinca@gmail.com>
2022-06-12 19:02:32 -0300

    rotas

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Dashboard } from '../pages/Dashboard';
+import { Repo } from '../pages/Repo';
+import { Route, Switch } from 'react-router-dom';
+
+export const Routes: React.FC = () => {
+  return (
+    <Switch>
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/repositories" component={Repo} />
+    </Switch>
+  );
+};
